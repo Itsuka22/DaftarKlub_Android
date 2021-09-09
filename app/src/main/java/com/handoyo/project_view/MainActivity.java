ba95e15c5148a9df5e9698e38374262b8defdfaf
Itsukahan <90296572+Itsuka22@users.noreply.github.com>
2021-09-10 01:49:37 +0700

    Update MainActivity.java
    change_commit

--- app/src/main/java/com/handoyo/project_view/MainActivity.java
+++ app/src/main/java/com/handoyo/project_view/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Project [] projects = {
                 new Project("Manchester United","Ini adalah klub Terbaik Liga Inggris Mempunyai gelar terbanyak, Optimis Trebel Winner GGMU", R.drawable.manchester),
                 new Project("Chelsea","Klub ini mempunyai julukan The Blues, yang kemarin baru saja menjuarai UCL. Semoga tidak bisa EPL tahun ini aokoakwokawk ", R.drawable.chelsea),
-                new Project("Arsenal","Klubnya Coach Justin ini (GOAHAHAHAHA) sangat serius untuk bersaing di dasar klasemen EPL. Bagaimana tidak?, dari 3 match aja belom pernah ngegolin", R.drawable.arsenal),
+                new Project("Arsenal","Klubnya Coach Justin ini (GOAHAHAHAHA) sangat serius untuk bersaing di dasar klasemen EPL (20).", R.drawable.arsenal),
                 new Project("Liverpool","Sudah tidak diragukan lagi, ya Liverpool atau The Reds, Tetap di obok2 king Ronaldo :)", R.drawable.liverpool),
                 new Project("Bayern Munchen","Klub berlokasi di Munich Jerman Memiliki segudang prestasi. Karena Bila ada pemain hebat di bundesliga, mesti pindahnya di Bayern", R.drawable.bayern),
 
